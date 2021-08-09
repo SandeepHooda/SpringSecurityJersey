860d5c15dd073d9f93e79c3012889892aaaeb6d4
SandeepHooda <sonu.hooda@gmail.com>
2021-08-09 18:09:09 +0530

    spring security

--- src/main/java/com/sandeep/security/springJersey/config/JerseyConfig.java
+++ src/main/java/com/sandeep/security/springJersey/config/JerseyConfig.java
@@ -3,14 +3,12 @@ package com.sandeep.security.springJersey.config;
 import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import com.sandeep.security.springJersey.endpoint.SayHello;
 import com.sandeep.security.springJersey.filter.SecurityFilter;
  
 @ApplicationPath("/api")
-@Configuration
 @Component
 public class JerseyConfig extends ResourceConfig 
 {
--- src/main/java/com/sandeep/security/springJersey/endpoint/SayHello.java
+++ src/main/java/com/sandeep/security/springJersey/endpoint/SayHello.java
@@ -10,12 +10,10 @@ import javax.ws.rs.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 
 import com.sandeep.security.springJersey.service.HelloService;
 
 @Path("/hello")
-@Component
 public class SayHello {
 	
 	@Autowired
